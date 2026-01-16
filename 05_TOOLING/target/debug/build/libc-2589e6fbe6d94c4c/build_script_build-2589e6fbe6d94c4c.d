771e69e39df2c4d7bd5017031f7d7f1767ce1a92
ib823 <ikmal.baharudin@gmail.com>
2026-01-16 14:09:23 +0000

    [CLEANUP] Add .gitignore and remove build artifacts from version control
    - Added proper .gitignore for 05_TOOLING directory
    - Removed target/ directory from git tracking
    - Build artifacts should not be version controlled
    
    This resolves uncommitted changes blocker for session end.

--- 05_TOOLING/target/debug/build/libc-2589e6fbe6d94c4c/build_script_build-2589e6fbe6d94c4c.d
+++
@@ -1,5 +0,0 @@
-/workspaces/proof/05_TOOLING/target/debug/build/libc-2589e6fbe6d94c4c/build_script_build-2589e6fbe6d94c4c: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.180/build.rs
-
-/workspaces/proof/05_TOOLING/target/debug/build/libc-2589e6fbe6d94c4c/build_script_build-2589e6fbe6d94c4c.d: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.180/build.rs
-
-/home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.180/build.rs:
--- 05_TOOLING/target/debug/build/num-traits-bcfe50daa15d0f5c/build_script_build-bcfe50daa15d0f5c.d
+++
@@ -1,5 +0,0 @@
-/workspaces/proof/05_TOOLING/target/debug/build/num-traits-bcfe50daa15d0f5c/build_script_build-bcfe50daa15d0f5c: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.19/build.rs
-
-/workspaces/proof/05_TOOLING/target/debug/build/num-traits-bcfe50daa15d0f5c/build_script_build-bcfe50daa15d0f5c.d: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.19/build.rs
-
-/home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.19/build.rs:
