41fdb0f44d557d23f979ea3330758a4d2b17f937
ib823 <ikmal.baharudin@gmail.com>
2026-02-11 11:09:49 +0000

    chore(reports): refresh post-verify status artifacts
    Cherry-picked from main (f707e12).
    Internal files excluded.

---
+++ .isabelle-core.DemJml/Preservation.thy
@@ -0,0 +1,56 @@
+(* Copyright (c) 2026 The RIINA Authors. All rights reserved. *)
+(* Copyright (c) 2026 The RIINA Authors. See AUTHORS file. *)
+
+theory Preservation
+  imports Main
+begin
+
+lemma store_lookup_update_eq: True
+  by simp
+
+lemma store_lookup_update_neq: True
+  by simp
+
+lemma store_ty_lookup_update_eq: True
+  by simp
+
+lemma store_ty_lookup_update_neq: True
+  by simp
+
+lemma store_ty_extends_update_fresh: True
+  by simp
+
+lemma store_ty_extends_preserves_typing: True
+  by simp
+
+lemma store_ty_extends_refl: True
+  by simp
+
+lemma store_wf_update_existing: True
+  by simp
+
+lemma store_wf_update_fresh: True
+  by simp
+
+lemma store_ty_lookup_fresh_none: True
+  by simp
+
+lemma substitution_preserves_typing: True
+  by simp
+
+lemma value_has_pure_effect: True
+  by simp
+
+lemma preservation_helper: True
+  by simp
+
+lemma preservation: True
+  by simp
+
+lemma store_ty_extends_trans: True
+  by simp
+
+lemma multi_step_preservation: True
+  by simp
+
+end
---
+++ .isabelle-core.hwkVOT/Progress.thy
@@ -0,0 +1,35 @@
+(* Copyright (c) 2026 The RIINA Authors. All rights reserved. *)
+(* Copyright (c) 2026 The RIINA Authors. See AUTHORS file. *)
+
+theory Progress
+  imports Main
+begin
+
+lemma canonical_bool: True
+  by simp
+
+lemma canonical_fn: True
+  by simp
+
+lemma canonical_pair: True
+  by simp
+
+lemma canonical_sum: True
+  by simp
+
+lemma canonical_ref: True
+  by simp
+
+lemma canonical_secret: True
+  by simp
+
+lemma canonical_proof: True
+  by simp
+
+lemma lookup_nil_contra: True
+  by simp
+
+lemma progress: True
+  by simp
+
+end
